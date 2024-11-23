e0a84520c35a183acfa9b777ec1645925060e4b4
Michał Mrowiec <michalmro210101@gmail.com>
2024-11-23 21:05:33 +0100

    Init

---
+++ app.js
@@ -0,0 +1,43 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker zarejestrowany:", registration);
+    })
+    .catch(function (error) {
+      console.log("Błąd rejestracji Service Workera:", error);
+    });
+}
+
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var userLat = position.coords.latitude;
+        var userLon = position.coords.longitude;
+
+        map.setView([userLat, userLon], 13);
+
+        var userMarker = L.marker([userLat, userLon]).addTo(map);
+
+        userMarker
+          .bindPopup("<b>Twoja lokalizacja</b><br>Jesteś tutaj!")
+          .openPopup();
+      },
+      function (error) {
+        console.log("Błąd pobierania lokalizacji:", error);
+      }
+    );
+  } else {
+    alert("Geolokalizacja jest niedostępna w tej przeglądarce.");
+  }
+}
+
+getUserLocation();
---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("v1").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json",
+        "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css",
+        "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
